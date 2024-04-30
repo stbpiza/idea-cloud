4a17ae800de35a09e44ac5edcc459df22ed9e155
stbpiza <stbpiza@gmail.com>
2024-04-30 23:31:16 +0900

    초기 프로젝트 생성

---
+++ src/main/java/org/ideacloud/IdeaCloudApplication.java
@@ -0,0 +1,13 @@
+package org.ideacloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdeaCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ideacloud/IdeaCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ideacloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
