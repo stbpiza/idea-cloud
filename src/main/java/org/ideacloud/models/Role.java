52c45a040773f266a0b81be8aaec797029614b73
stbpiza <stbpiza@gmail.com>
2024-05-15 23:41:19 +0900

    시큐리티 구현중

---
+++ src/main/java/org/ideacloud/models/Role.java
@@ -0,0 +1,17 @@
+package org.ideacloud.models;
+
+public enum Role {
+    ROLE_USER("ROLE_USER"),
+    ROLE_ADMIN("ROLE_ADMIN")
+    ;
+
+    private final String value;
+
+    Role(String value) {
+        this.value = value;
+    }
+
+    public String toString() {
+        return value;
+    }
+}
---
+++ src/main/java/org/ideacloud/security/AuthUser.java
@@ -0,0 +1,25 @@
+package org.ideacloud.security;
+
+import org.ideacloud.models.Role;
+
+public record AuthUser(
+        String id,
+        String email,
+        String password,
+        String role,
+        String accessToken
+) {
+    public static AuthUser of(
+            String id, String email, String password, String role) {
+        return new AuthUser(id, email, password, role, "");
+    }
+
+    public static AuthUser authenticated(
+            String id, String role, String accessToken) {
+        return new AuthUser(id, "", "", role, accessToken);
+    }
+
+    public boolean isAdmin() {
+        return Role.valueOf(role).equals(Role.ROLE_ADMIN);
+    }
+}
