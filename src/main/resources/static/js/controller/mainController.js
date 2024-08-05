b41cd129098c829cc18cfe056e5df3bb34b3cd75
stbpiza <stbpiza@gmail.com>
2024-08-05 23:43:12 +0900

    [프론트] 홈 화면 API 응답 데이터로 화면 그리기까지 구현

--- src/main/resources/static/js/controller/mainController.js
+++ src/main/resources/static/js/controller/mainController.js
@@ -1,5 +1,17 @@
 import { get, post } from './controller.js';
+import { drawMeetingNoteList } from "../view/homeView.js";
 
+let page = 0;
+
+async function meetingNoteList() {
+
+    let body = await meetingNoteListRequest(page);
+
+    console.log(body);
+
+    drawMeetingNoteList(body);
+
+}
 
 
 
@@ -8,11 +20,13 @@ function meetingNoteListRequest(page) {
     console.log(page);
     return get('/api/meeting-notes', `?page=${page}&size=10`)
         .then(response => {
-            console.log(response.json());
+            console.log(response);
+            return response.json();
         })
-        .catch(error => {
-            console.log(error);
+        .then(data => {
+            console.log(data);
+            return data;
         })
 }
 
-meetingNoteListRequest(1);
+meetingNoteList();
