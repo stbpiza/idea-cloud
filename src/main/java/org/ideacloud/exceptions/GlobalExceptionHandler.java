d6b7b3c4820e7f255e3bbde11b9497a213a17263
stbpiza <stbpiza@gmail.com>
2024-12-17 23:44:40 +0900

    GlobalExceptionHandler 예외처리 응답, 로그 개선

--- src/main/java/org/ideacloud/exceptions/GlobalExceptionHandler.java
+++ src/main/java/org/ideacloud/exceptions/GlobalExceptionHandler.java
@@ -1,7 +1,11 @@
 package org.ideacloud.exceptions;
 
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.convert.ConversionFailedException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -40,5 +44,68 @@ public class GlobalExceptionHandler {
                 ErrorCode.UNAUTHORIZED,
                 e.getMessage()
         );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDto handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.error("Method argument not valid: {}", e.getMessage());
+        return new ErrorDto(
+                ErrorCode.BAD_REQUEST,
+                e.getMessage()
+        );
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorDto handleException(Exception e) {
+        log.error("Internal server error: {}", e.getMessage());
+        log.error(getErrorLine(e.getStackTrace()));
+        return new ErrorDto(
+                ErrorCode.DATABASE_ERROR,
+                e.getMessage()
+        );
+    }
+
+    // TODO: 나중에 필요하면 주석 해제
+//    @ExceptionHandler(ConstraintViolationException.class)
+//    @ResponseStatus(HttpStatus.BAD_REQUEST)
+//    public ErrorDto handleConstraintViolationException(ConstraintViolationException e) {
+//        log.error("Constraint violation: {}", e.getMessage());
+//        return new ErrorDto(
+//                ErrorCode.BAD_REQUEST,
+//                e.getMessage()
+//        );
+//    }
+//
+//    @ExceptionHandler(ConversionFailedException.class)
+//    @ResponseStatus(HttpStatus.BAD_REQUEST)
+//    public ErrorDto handleConversionFailedException(ConversionFailedException e) {
+//        log.error("Conversion failed: {}", e.getMessage());
+//        return new ErrorDto(
+//                ErrorCode.BAD_REQUEST,
+//                e.getMessage()
+//        );
+//    }
+//
+//    @ExceptionHandler(InvalidFormatException.class)
+//    @ResponseStatus(HttpStatus.BAD_REQUEST)
+//    public ErrorDto handleInvalidFormatException(InvalidFormatException e) {
+//        log.error("Invalid format: {}", e.getMessage());
+//        return new ErrorDto(
+//                ErrorCode.BAD_REQUEST,
+//                e.getMessage()
+//        );
+//    }
+
+
+    protected String getErrorLine(StackTraceElement[] stackTrace) {
+        StringBuilder errorLog = new StringBuilder();
+        for (StackTraceElement stackTraceElement : stackTrace) {
+            if (stackTraceElement.getClassName().contains("org.ideacloud")) {
+                errorLog.append(stackTraceElement).append("\n");
+            }
+        }
+        return errorLog.toString();
     }
 }
