25d07c93244466ad0e84eaf620d6f64857cce537
stbpiza <stbpiza@gmail.com>
2025-02-18 23:33:51 +0900

    키워드 업데이트 로직 추가

--- src/main/java/org/ideacloud/dtos/MeetingNoteUpdateDto.java
+++ src/main/java/org/ideacloud/dtos/MeetingNoteUpdateDto.java
@@ -1,23 +1,11 @@
 package org.ideacloud.dtos;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-
 import java.util.List;
 
 public record MeetingNoteUpdateDto(
         Long id,
         String title,
         String body,
-        List<UpdateKeywordToMeetingNoteDto> keywords
+        List<MeetingNoteCreateDto.AddKeywordToMeetingNoteDto> keywords
 ) {
-
-    public record UpdateKeywordToMeetingNoteDto(
-            Long id,
-            @NotBlank
-            String keyword,
-            @Min(1)
-            int count
-    ){
-    }
 }
